79f404139eb2ea49ab37aceb65241353cd7b7502
JosephLopB <joshyngppo@gmail.com>
2025-07-31 13:31:46 -0500

    Operadores
    Ejercicio terminado

---
+++ Joseph 3168940/2_operadores_comparación_logico/app.js
@@ -0,0 +1,13 @@
+alert ( " Dame 2 números ")
+
+let num1 = prompt( 'Valor número 1')
+let num2 = prompt( 'Valor número 2')
+
+let resultado
+
+/* console.log( "El resultado de la suma de ambos numeros es" + " " + z )
+console.log( "El resultado de la resta de ambos numeros es" + " " + z )
+console.log( "El resultado de la multiplicación de ambos numeros es" + " " + z )
+console.log( "El resultado de la división de ambos numeros es" + " " + z ) */
+
+console.log(`Tu número es ${}`)
