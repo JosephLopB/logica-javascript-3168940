f9e4a81c981756367ad950a32b8db8ccb645d72a
JosephLopB <joshyngppo@gmail.com>
2025-08-04 13:32:26 -0500

    Add files via upload
    Actividades condicionales

---
+++ Joseph 3168940/Joseph 3168940/3_condicionales/app.js
@@ -0,0 +1,10 @@
+alert ("Control de ingreso a la disco 😎");
+
+let Nombre = (prompt("Escibe tu Nombre"));
+let edad = Number (prompt("Escribe tu edad"));
+
+if (edad > 18) {
+    alert("ingresa sin problema")
+} else {
+    alert("No ingresas");
+}
