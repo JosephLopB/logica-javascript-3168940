8d6991b4e332b72d737b424a806ad221256b2f62
JosephLopB <joshyngppo@gmail.com>
2025-08-04 12:12:33 -0500

    Add files via upload
    Corrección de novedad con la lectura de las variables como números

---
+++ Joseph 3168940/Joseph 3168940/2_operadores_comparación_logico/app.js
@@ -0,0 +1,18 @@
+alert ( " Dame 2 números ");
+
+let num1 = Number( prompt( 'Valor número 1') )
+let num2 = Number( prompt( 'Valor número 2') )
+
+let suma = num1 + num2;
+let resultado1 = num1 - num2;
+let resultado2 = num1 * num2;
+let resultado3 = num1 / num2;
+
+
+
+alert( "El resultado de la suma de ambos numeros es" + " " + suma )
+alert( "El resultado de la resta de ambos numeros es" + " " + resultado1 )
+alert( "El resultado de la multiplicación de ambos numeros es" + " " + resultado2 )
+alert( "El resultado de la división de ambos numeros es" + " " + resultado3 )
+
+/* console.log(`Tu número es ${}`) */
