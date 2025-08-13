64e0e5aebca64c488ad4d05b35130cdb6c1d266c
JosephLopB <joshyngppo@gmail.com>
2025-08-13 11:59:40 -0500

    Home work
    Ejercicios de array y math sesion 3

--- 4_ciclos_arrays/app.js
+++ 4_ciclos_arrays/app.js
@@ -1,13 +1,1 @@
-//Array con "[]"
-let letras = ["a","b","c","d"];
-
-//Objeto con "{}"
-let movie = {
-    nombre: "Back to te future",
-    estreno: 1986,
-    director: "Robert Zemeckis",
-    calificacion: "8.5",
-}
-
- console.log(letras[2])
- console.log(movie.estreno)
+app.js
